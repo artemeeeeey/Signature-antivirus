c2af41358e3ac3ffcfde43932574802a30d41527
SevaSeva17 <112716693+SevaSeva17@users.noreply.github.com>
2025-05-03 21:28:27 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,308 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//FIMOZ RULIT!!
-void
-hexdump (unsigned long bse, char *buf, int len)
-{
-  int pos;
-  char line[80];
-  
-  while (len > 0)
-    {
-      int cnt, i;
+#include <stdint.h>
+#include <ctype.h>
+#include <errno.h>
 
-      pos = 0;
-      cnt = 16;
-      if (cnt > len)
-        cnt = len;
+#define A 0x67452301
+#define B 0xefcdab89
+#define C 0x98badcfe
+#define D 0x10325476
+#define MAX_PATTERNS 102000
+#define MAX_PATTERN_LEN 1024
+#define BUFFER_SIZE (10 * 1024 * 20)
 
-      for (i = 0; i < cnt; i++)
-        {
-          pos += snprintf (&line[pos], sizeof (line) - pos,
-                                "%02x ", (unsigned char) buf[i]);
+typedef struct {
+    uint64_t size;        // Size of input in bytes
+    uint32_t buffer[4];   // Current accumulation of hash
+    uint8_t input[64];    // Input to be used in the next step
+    uint8_t digest[16];   // Result of algorithm
+} MD5Context;
+
+typedef struct {
+    uint8_t data[MAX_PATTERN_LEN];
+    uint8_t mask[MAX_PATTERN_LEN];
+    size_t length;
+    char original[512];
+} BytePattern;
+
+BytePattern patterns[MAX_PATTERNS];
+int num_patterns = 0;
+
+static uint32_t S[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                       5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+                       4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                       6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+
+static uint32_t K[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+                       0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+                       0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+                       0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+                       0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+                       0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+                       0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+                       0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+                       0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+                       0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+                       0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+                       0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+                       0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+                       0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+                       0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+                       0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
+
+static uint8_t PADDING[] = {0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+#define F(X, Y, Z) ((X & Y) | (~X & Z))
+#define G(X, Y, Z) ((X & Z) | (Y & ~Z))
+#define H(X, Y, Z) (X ^ Y ^ Z)
+#define I(X, Y, Z) (Y ^ (X | ~Z))
+
+uint32_t rotateLeft(uint32_t x, uint32_t n) {
+    return (x << n) | (x >> (32 - n));
+}
+
+void md5Step(uint32_t *buffer, uint32_t *input) {
+    uint32_t AA = buffer[0];
+    uint32_t BB = buffer[1];
+    uint32_t CC = buffer[2];
+    uint32_t DD = buffer[3];
+    uint32_t E;
+    unsigned int j;
+
+    for (unsigned int i = 0; i < 64; ++i) {
+        switch (i / 16) {
+            case 0: E = F(BB, CC, DD); j = i; break;
+            case 1: E = G(BB, CC, DD); j = ((i * 5) + 1) % 16; break;
+            case 2: E = H(BB, CC, DD); j = ((i * 3) + 5) % 16; break;
+            default: E = I(BB, CC, DD); j = (i * 7) % 16; break;
         }
-      printf ("%s\n", line);
 
-      bse += 16;
-      buf += 16;
-      len -= cnt;
+        uint32_t temp = DD;
+        DD = CC;
+        CC = BB;
+        BB = BB + rotateLeft(AA + E + K[i] + input[j], S[i]);
+        AA = temp;
+    }
+
+    buffer[0] += AA;
+    buffer[1] += BB;
+    buffer[2] += CC;
+    buffer[3] += DD;
+}
+
+void md5Init(MD5Context *ctx) {
+    ctx->size = 0;
+    ctx->buffer[0] = A;
+    ctx->buffer[1] = B;
+    ctx->buffer[2] = C;
+    ctx->buffer[3] = D;
+}
+
+void md5Update(MD5Context *ctx, uint8_t *input_buffer, size_t input_len) {
+    uint32_t input[16];
+    unsigned int offset = ctx->size % 64;
+    ctx->size += input_len;
+
+    for (unsigned int i = 0; i < input_len; ++i) {
+        ctx->input[offset++] = input_buffer[i];
+
+        if (offset % 64 == 0) {
+            for (unsigned int j = 0; j < 16; ++j) {
+                input[j] = (uint32_t)(ctx->input[(j * 4) + 3]) << 24 |
+                           (uint32_t)(ctx->input[(j * 4) + 2]) << 16 |
+                           (uint32_t)(ctx->input[(j * 4) + 1]) << 8 |
+                           (uint32_t)(ctx->input[(j * 4)]);
+            }
+            md5Step(ctx->buffer, input);
+            offset = 0;
+        }
     }
 }
 
-int main(int argc, char* argv[]){
-    FILE *f = fopen(argv[1], "r");
-    char k1ll3d[800];
-    while (!feof(f)){
-        fgets(k1ll3d, sizeof(k1ll3d), f);
-        hexdump(16, k1ll3d, sizeof(k1ll3d));
+void md5Finalize(MD5Context *ctx) {
+    uint32_t input[16];
+    unsigned int offset = ctx->size % 64;
+    unsigned int padding_length = offset < 56 ? 56 - offset : (56 + 64) - offset;
+
+    md5Update(ctx, PADDING, padding_length);
+    ctx->size -= padding_length;
+
+    for (unsigned int j = 0; j < 14; ++j) {
+        input[j] = (uint32_t)(ctx->input[(j * 4) + 3]) << 24 |
+                   (uint32_t)(ctx->input[(j * 4) + 2]) << 16 |
+                   (uint32_t)(ctx->input[(j * 4) + 1]) << 8 |
+                   (uint32_t)(ctx->input[(j * 4)]);
     }
+    input[14] = (uint32_t)(ctx->size * 8);
+    input[15] = (uint32_t)((ctx->size * 8) >> 32);
+
+    md5Step(ctx->buffer, input);
+
+    for (unsigned int i = 0; i < 4; ++i) {
+        ctx->digest[(i * 4) + 0] = (uint8_t)((ctx->buffer[i] & 0x000000FF));
+        ctx->digest[(i * 4) + 1] = (uint8_t)((ctx->buffer[i] & 0x0000FF00) >> 8);
+        ctx->digest[(i * 4) + 2] = (uint8_t)((ctx->buffer[i] & 0x00FF0000) >> 16);
+        ctx->digest[(i * 4) + 3] = (uint8_t)((ctx->buffer[i] & 0xFF000000) >> 24);
+    }
+}
+
+void hexdump(char *buf, int len) {
+    int pos;
+    char line[80];
+    while (len > 0) {
+        int cnt = (len > 16) ? 16 : len;
+        pos = 0;
+
+        for (int i = 0; i < cnt; i++) {
+            pos += snprintf(&line[pos], sizeof(line) - pos, "%02x", (unsigned char)buf[i]);
+        }
+        //printf("%s\n", line);
+
+        buf += cnt;
+        len -= cnt;
+    }
+}
+
+char* ya_gomoseksualist(uint8_t *hash) {
+    int pos=0;
+    char *result = (char*)malloc(33);
+    for (int i = 0; i < 32; i++) {
+        pos += snprintf(&result[pos], 33 - pos, "%02x", hash[i]);
+    }
+    result[32] = '\0';
+    return result;
+}
+
+int parse_pattern(const char *str, BytePattern *pattern) {
+    size_t len = 0;
+    size_t i = 0;
+    memset(pattern, 0, sizeof(BytePattern));
+    strncpy(pattern->original, str, sizeof(pattern->original));
+
+    while (str[i] && len < MAX_PATTERN_LEN) {
+        if (isspace(str[i])) {
+            i++;
+            continue;
+        }
+
+        if (str[i] == '?' && str[i+1] == '?') {
+            pattern->mask[len] = 0;
+            i += 2;
+            len++;
+        } 
+        else if (isxdigit(str[i]) && isxdigit(str[i+1])) {
+            char byte_str[3] = {str[i], str[i+1], '\0'};
+            if (sscanf(byte_str, "%2hhx", &pattern->data[len]) != 1) {
+                return 0;
+            }
+            pattern->mask[len] = 1;
+            i += 2;
+            len++;
+        } else {
+            return 0;
+        }
+    }
+    pattern->length = len;
+    return len > 0;
+}
+
+int load_patterns(const char *filename) {
+    FILE *fp = fopen(filename, "r");
+
+    char line[512];
+    while (fgets(line, sizeof(line), fp) && num_patterns < MAX_PATTERNS) {
+        line[strcspn(line, "\r\n")] = 0;
+        if (parse_pattern(line, &patterns[num_patterns])) {
+            num_patterns++;
+        }
+    }
+    fclose(fp);
+    return 1;
+}
+
+int find_pattern(const uint8_t *buffer, size_t buf_len, const BytePattern *pattern) {
+    if (pattern->length == 0 || buf_len < pattern->length) return 0;
+    
+    const size_t limit = buf_len - pattern->length;
+    for (size_t i = 0; i <= limit; i++) {
+        int match = 1;
+        for (size_t j = 0; j < pattern->length; j++) {
+            if (pattern->mask[j] && buffer[i + j] != pattern->data[j]) {
+                match = 0;
+                break;
+            }
+        }
+        if (match) {
+            //printf("len is %d\n", pattern->length);
+            //printf("num_patterns is %d\n", num_patterns);
+            printf("match at address: 0x%08zx\n", i);
+            printf("found pattern: %s\n", pattern->original);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *f = fopen(argv[1], "rb");
+
+    uint8_t buffer[640000];
+    uint8_t hash[16];
+
+    size_t bytes_read = fread(buffer, 1, sizeof(buffer), f);
+
+    MD5Context ctx;
+    md5Init(&ctx);
+    md5Update(&ctx, buffer, bytes_read);
+    md5Finalize(&ctx);
+    memcpy(hash, ctx.digest, 16);
+    hexdump((char *)buffer, bytes_read);
+    char* result = ya_gomoseksualist(hash);
+    fclose(f);
+
+
+    FILE* file = fopen("hash.txt", "a+");
+    fseek(file, 0, SEEK_SET);
+
+    char buff[256];
+    printf("result is %s\n", result);
+
+    while (fgets(buff, sizeof(buff), file)) {
+        if (strncmp(buff, result, 32)==0){
+            fclose(file);
+            printf("ur file seems to be virus file\n");
+            return 0;
+        }
+    }
+    fclose(file);
+
+    
+    load_patterns("bytes.txt");
+    uint8_t *file_content = malloc(BUFFER_SIZE);
+
+    FILE *target_file = fopen(argv[1], "rb");
+    size_t bytes_r = fread(file_content, 1, BUFFER_SIZE, target_file);
+    fclose(target_file);
+
+    int found = 0;
+    for (int i = 0; i < num_patterns; i++) {
+        if (find_pattern(file_content, bytes_r, &patterns[i])) {
+            free(file_content);
+            printf("file contains malicious byte pattern\n");
+            return 0;
+        }
+    }
+
+    free(file_content);
+    printf("file is safe\n");
     return 0;
 }
